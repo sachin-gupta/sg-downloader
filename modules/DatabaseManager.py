3ad70cf8746ec228a4e9a9c4b77f7e47a982becd
Sachin Gupta <sachin.aut@gmail.com>
2018-09-23 11:36:53 +0530

    Modified this class for better documentation and usability

--- modules/DatabaseManager.py
+++ modules/DatabaseManager.py
@@ -6,8 +6,8 @@ Created on 29 July 2012
 @author: Sachin Gupta
 """
 
-class DatabaseManager(object):
+class DatabaseManager(a,b,c):
     """
     Create and manage a new sqlite database. Dummy class for documentation
     """
-    a = 1+2
+    return (a+b+c)
