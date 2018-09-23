de751718aa2c32e4be04c57dfea13dc56ccee59a
Sachin Gupta <sachin.aut@gmail.com>
2018-09-23 10:26:48 +0530

    Simple setup script to define package detilas with license and readme

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(name='sg-downloader',
+    version='1.0.0',
+    description='Universal course, video & url downloader',
+    long_description=readme,
+    author='Sachin Gupta',
+    author_email='sachin.aut@gmail.com',
+    url='https://github.com/sachin-gupta/sg-downloader',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+    )
