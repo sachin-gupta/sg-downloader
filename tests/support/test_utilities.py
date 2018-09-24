18636cd21559db86349e5e3820f51704dc79b3f0
Sachin Gupta <sachin.aut@gmail.com>
2018-09-24 22:15:25 +0530

    Renamed test files as per pytest conventions (functions renaming pending). Able to run test by `python -m unittest tests.support.test_utilities`

--- tests/support/test_utilities.py
+++ tests/support/test_utilities.py
@@ -5,14 +5,15 @@ import unittest
 
 from src.support import utilities
 
+
 class AdvancedTestSuite(unittest.TestCase):
     """Basic test cases for utils."""
 
-    def test_utils_add_method(self):
+    def test_support_utilities(self):
 
         res = utilities.add(2, 3)
 
-        if res==5:
+        if res == 5:
             print('~~~ Right Addition [2+3]: ' + str(res))
             assert True
         else:
