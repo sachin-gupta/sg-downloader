979f939b80c9d6c4c7eb8ab70236573b7f698ce2
Sachin Gupta <sachin.aut@gmail.com>
2018-09-24 08:26:27 +0530

    Now building docs with package, Generating 3 distros - binary windows, source and universal wheel

--- buildpkg.bat
+++ buildpkg.bat
@@ -1,4 +1,7 @@
+@ECHO Calling build of documentation
+CALL builddocs.bat
+
 @ECHO This batch file will build package for this project using Sphinx
 rm -rf dist/
 rm -rf sg_downloader.egg-info/
-python setup.py sdist
+python setup.py sdist bdist bdist_wheel --universal
